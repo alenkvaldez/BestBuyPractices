091ee175a840d9b41b36942a4ca98b6a007d10b0
Alen Valdez <116237490+alenkvaldez@users.noreply.github.com>
2023-03-05 13:46:30 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using BestBuyPractices;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+var departmentRepo = new DapperDepartmentRepository(conn);
+
+departmentRepo.InsertDepartment("alemans new department");
+
+var departments = departmentRepo.GetAllDepartments();
+
+foreach  (var department in departments)
+{
+    Console.Write(department.Name);
+    Console.WriteLine(department.Name);
+    Console.WriteLine();
+    Console.WriteLine();
+}
